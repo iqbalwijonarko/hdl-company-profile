647e77fb0db4a4b28350a38f6cf67f32c1c4d66b
gafiirfandi <gafiirfandi45@gmail.com>
2023-01-30 13:19:04 +0700

    initialize products and services page

---
+++ src/pages/products-and-services/index.tsx
@@ -0,0 +1,12 @@
+import ContactUsInfo from "@/components/ContactUsInfo/ContactUsInfo";
+
+export default function ProductsAndServices() {
+  return (
+    <div className="products-and-services">
+      <div className="products-and-services__landing-container">
+      </div>
+      
+      <ContactUsInfo />
+    </div>
+  );
+}
